60d3d9c5501e4d9e98af525618cb8b42bb692a31
TinyHai <1035908357@qq.com>
2024-04-23 21:40:50 +0800

    add AuthActivity and AuthViewModel

---
+++ app/src/main/java/cn/tinyhai/ban_uninstall/vm/AuthViewModel.kt
@@ -0,0 +1,54 @@
+package cn.tinyhai.ban_uninstall.vm
+
+import androidx.lifecycle.ViewModel
+import cn.tinyhai.ban_uninstall.auth.client.AuthClient
+import cn.tinyhai.ban_uninstall.auth.entites.AuthData
+
+class AuthViewModel : ViewModel() {
+
+    companion object {
+        private const val TAG = "AuthViewModel"
+    }
+
+    private var handled: Boolean = false
+
+    private lateinit var authClient: AuthClient
+
+    lateinit var authData: AuthData
+        private set
+
+    val hasPwd get() = authClient.hasPwd
+
+    fun setup(authClient: AuthClient, authData: AuthData) {
+        this.authClient = authClient
+        this.authData = authData
+    }
+
+    fun onAgree() {
+        if (handled) {
+            return
+        }
+        authClient.agree(authData.opId)
+        handled = true
+    }
+
+    fun onPrevent() {
+        if (handled) {
+            return
+        }
+        authClient.prevent(authData.opId)
+        handled = true
+    }
+
+    fun authenticate(pwd: String): Boolean {
+        return authClient.authenticate(pwd)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        if (!handled) {
+            onPrevent()
+            handled = true
+        }
+    }
+}
