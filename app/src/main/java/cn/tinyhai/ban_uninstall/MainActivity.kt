a9d3f4beb64a192c8368994c4f110e3df351f0ed
TinyHai <1035908357@qq.com>
2024-04-23 22:07:22 +0800

    refactor some code

--- app/src/main/java/cn/tinyhai/ban_uninstall/MainActivity.kt
+++ app/src/main/java/cn/tinyhai/ban_uninstall/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("MainActivity", intent.toString())
-        TransactClient.init(intent)
+        TransactClient.inject(intent)
         enableEdgeToEdge()
         setContent {
             AppTheme {
