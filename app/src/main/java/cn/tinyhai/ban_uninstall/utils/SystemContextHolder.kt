92a0f5989616f5df90701fedc9aaeec65ef0c428
TinyHai <1035908357@qq.com>
2024-04-23 21:41:53 +0800

    add SystemContextHolder

---
+++ app/src/main/java/cn/tinyhai/ban_uninstall/utils/SystemContextHolder.kt
@@ -0,0 +1,36 @@
+package cn.tinyhai.ban_uninstall.utils
+
+import android.annotation.SuppressLint
+import android.content.Context
+
+@SuppressLint("StaticFieldLeak")
+object SystemContextHolder {
+    private val callbacks = ArrayList<Callback>()
+
+    private var systemContext: Context? = null
+    val context: Context get() = systemContext ?: throw IllegalStateException()
+
+    fun withSystemContext(block: Context.() -> Unit) {
+        context.block()
+    }
+
+    fun onSystemContext(context: Context) {
+        this.systemContext = context
+        val callbacks = callbacks.toList().also { callbacks.clear() }
+        callbacks.forEach {
+            it.onSystemContext(context)
+        }
+    }
+
+    fun registerCallback(callback: Callback) {
+        if (systemContext == null) {
+            callbacks.add(callback)
+        } else {
+            callback.onSystemContext(context)
+        }
+    }
+
+    fun interface Callback {
+        fun onSystemContext(context: Context)
+    }
+}
