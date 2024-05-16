9fe95c070db474e30259f003bf8d76e380781b3b
TinyHai <1035908357@qq.com>
2024-05-16 16:15:29 +0800

    refine bugreport

--- app/src/main/java/cn/tinyhai/ban_uninstall/utils/Bugreport.kt
+++ app/src/main/java/cn/tinyhai/ban_uninstall/utils/Bugreport.kt
@@ -10,12 +10,30 @@ import java.util.GregorianCalendar
 
 @SuppressLint("SimpleDateFormat")
 suspend fun getLogcatFile() = withContext(Dispatchers.IO) {
+    val cacheDir = App.app.cacheDir
+    val bugreportDir = File(cacheDir, "bugreport").also { it.mkdirs() }
     val time = GregorianCalendar.getInstance().time
     val formatter = SimpleDateFormat("yyyy-MM-dd_HH_mm")
-    val logcatFile = File(App.app.cacheDir, "Logcat_${formatter.format(time)}.txt")
+    val logcatFile = File(bugreportDir, "logcat.txt")
     fastResultWithRootShell("logcat", "-d", ">", logcatFile.absolutePath)
-    if (!logcatFile.exists()) {
-        logcatFile.createNewFile()
-    }
-    logcatFile
+    val bugreportFile = File(cacheDir, "BanUninstall_bugreport_${formatter.format(time)}.tar.gz")
+    fastResultWithRootShell(
+        "tar",
+        "-czf",
+        bugreportFile.absolutePath,
+        "-C",
+        bugreportDir.absolutePath,
+        "."
+    )
+    fastResultWithRootShell(
+        "rm",
+        "-rf",
+        bugreportDir.absolutePath
+    )
+    fastResultWithRootShell(
+        "chmod",
+        "0644",
+        bugreportFile.absolutePath
+    )
+    bugreportFile
 }
