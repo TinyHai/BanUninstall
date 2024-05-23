e51b1762171b02d5be18fbeddf588d0d75e020bf
TinyHai <1035908357@qq.com>
2024-05-23 17:58:12 +0800

    fix flags type

--- hiddenApi/src/main/java/android/content/pm/IPackageManager.java
+++ hiddenApi/src/main/java/android/content/pm/IPackageManager.java
@@ -6,6 +6,8 @@ public interface IPackageManager extends android.os.IInterface {
 
     PackageInfo getPackageInfo(String packageName, long flags, int userId);
 
+    ApplicationInfo getApplicationInfo(String packageName, long flags, int userId);
+
     abstract class Stub extends android.os.Binder implements android.content.pm.IPackageManager {
         public static android.content.pm.IPackageManager asInterface(android.os.IBinder obj) {
             throw new UnsupportedOperationException();
