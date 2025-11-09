2b46045ae8500069f011418b35aa894fd0283b49
TinyHai <1035908357@qq.com>
2025-11-09 19:34:29 +0800

    bump to v1.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "cn.tinyhai.ban_uninstall"
         minSdk = 21
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.4.1"
+        versionCode = 10
+        versionName = "1.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
